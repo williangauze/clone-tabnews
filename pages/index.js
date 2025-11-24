f4416166f44bad4f3a3ec22ae822d5db56a95050
Willian <williangauze@gmail.com>
2025-11-24 22:01:30 +0000

    mensagem lubson

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 function Home () {
-    return <h1> Mãe, estou aprendendo a fazer um site.
-        Logo logo estou PRO para fazer o site da Ale gourmet. 2026 será o melhor ano!  </h1>   
+    return <h1> Lubson, estou com saudade de você. A vida anda corrida, 
+        mas queria que soubesse 
+        que sinto falta das nossas 
+        conversas e da nossa parceria de sempre. 
+        Quando der, vamos nos ver e colocar tudo em dia. 
+        Você é importante pra mim. </h1>   
 }
 
 export default Home;
