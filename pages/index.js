58c6986d121d3ce5fdcdc62e260f905b35f77353
Willian <williangauze@gmail.com>
2025-11-24 21:59:16 +0000

    nova mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 function Home () {
-    return <h1> Bruno, muito obrigado por ter me emprestado o dinheiro. Sexta feira eu te devolvo ele integral.
-        Eu tenho certeza que o Mapa do Leilão vai dar certo em nossas mãos e por isso estou te mandando essa página
-        Tenha certeza que vamos fazer muitos projetos juntos e que nossa sociedade vai longe! Abraços.  </h1>   
+    return <h1> Mãe, estou aprendendo a fazer um site.
+        Logo logo estou PRO para fazer o site da Ale gourmet. 2026 será o melhor ano!  </h1>   
 }
 
 export default Home;
