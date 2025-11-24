94a30c1f021ec978af68e22367ab3ac2475787de
Willian <williangauze@gmail.com>
2025-11-24 21:52:04 +0000

    nova mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home () {
-    return <h1> Rebeca, eu te amo. Se você me ama da uma risadinha! Miau  </h1>   
+    return <h1> Bruno, muito obrigado por ter me emprestado o dinheiro. Sexta feira eu te devolvo ele integral.
+        Eu tenho certeza que o Mapa do Leilão vai dar certo em nossas mãos e por isso estou te mandando essa página
+        Tenha certeza que vamos fazer muitos projetos juntos e que nossa sociedade vai longe! Abraços.  </h1>   
 }
 
 export default Home;
