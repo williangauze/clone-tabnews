4119cc0effd163c3038f7fc1ae4282603afc4241
Willian <williangauze@gmail.com>
2025-11-26 19:32:32 +0000

    mensagem rocha

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
 function Home () {
-    return <h1> Bom dia, venho através desse comunicado que estou no
-        aguardo da alteração do limite para realizar o pagamento conforme combinado.
-        Desde já agradeço a compreensão.
-        
-        Atenciosamente, </h1>   
+    return <h1> Salve meu amigo, tudo bem? Está de bobeira hoje? </h1>   
 }
 
 export default Home;
